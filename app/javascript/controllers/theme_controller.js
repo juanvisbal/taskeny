ad8af648da2aa9ce0294beca84815cf92c9ccc95
Juan Visbal <955241+juanvisbal@users.noreply.github.com>
2025-04-29 19:02:32 -0500

    feat: dark mode toggle

---
+++ app/javascript/controllers/theme_controller.js
@@ -0,0 +1,27 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = []
+
+  connect() {
+    // Check for saved theme preference or use the system preference
+    if (localStorage.theme === 'dark' || 
+        (!('theme' in localStorage) && 
+         window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+  }
+
+  toggle() {
+    // Toggle the theme
+    if (document.documentElement.classList.contains('dark')) {
+      document.documentElement.classList.remove('dark')
+      localStorage.theme = 'light'
+    } else {
+      document.documentElement.classList.add('dark')
+      localStorage.theme = 'dark'
+    }
+  }
+}
